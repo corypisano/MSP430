d77a205f75ad748831de80abcca888c7075df175
cory.pisano <cory.pisano@gmail.com>
2014-04-16 18:37:33 -0400

    Instructions changed, new method to generate tone with consistent sampling rate

--- MusicGeneration/main.c
+++ MusicGeneration/main.c
@@ -8,63 +8,55 @@ Project 2 - Tone and Music Generation
 Assignment Instructions:
      . . .
 
-
-
-
 Switching between modes - interrupt driven? or in while(1) { }
  */
 
 #include <msp430.h>
 
 /* globals */
-#define INPUT BIT3
-#define LEDR BIT0
-#define LEDG BIT6
-#define MESSAGE_LENGTH 5
+#define INPUT 		BIT3
+#define LEDR 		BIT0
+#define LEDG 		BIT6
+#define CLOCK_F 	16000000
+#define BIN_WIDTH 	255
+#define FREQ_SCALAR	CLOCK_F/(4*BIN_WIDTH)
 
-void playTone(unsigned int frequency, unsigned int duration) {
+//volatile unsigned int volume 	= 8;  // inverse volume (2 is max, 255 is min)
+volatile unsigned int toneCount = 36; //
+volatile unsigned int count 	= 0;	  //
 
-	TA0CCR0 = (4000000/frequency) - 1;
-	TA0CCR1 = 4;
-
-	// switch to duration using Timer
-	__delay_cycles(4000000);
-
-	// TA0CCR0 = 0; // turn note off?
-}
-
-void stopTone() {
-	TA0CCR0 = 0;
-	TA0CCR1 = 0;
-}
-
-void MODE_1() {
-	/* Play A4 */
-	playTone(440, 16000); // A4  = 440Hz
-}
-
-void MODE_2() {
-	/* Play A major! */
-	playTone(440, 16000); // A4  = 440Hz
-	playTone(494, 16000); // B4  = 494Hz
-	playTone(554, 16000); // C5# = 554Hz
-	playTone(587, 16000); // D5  = 587Hz
-	playTone(659, 16000); // E5  = 659Hz
-	playTone(740, 16000); // F5# = 740Hz
-	playTone(831, 16000); // G5# = 831Hz
-}
-
-void MODE_3() {
-	/* Play a song! */
-	playTone(440, 16000); // A4  = 440Hz
-	playTone(494, 16000); // B4  = 494Hz
-}
-
-/* Timer A timeout interrupt */
+/* Timer A0 timeout interrupt
+ * occurs when TAR == BIN_WIDTH
+ *
+ */
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void TimerA0 (void) {
+
+	count++;
+
+	/* SET TA0CCR1 */
+	if (count < toneCount/2) {
+		TA0CCR1 = 255;
+	}
+	else if (count < toneCount) {
+		TA0CCR1 = 0;
+	}
+
+	/* Reset count */
+	if (count == toneCount) {
+		count = 0;
+	}
+
+	TA0CTL &= ~TAIFG;
 }
 
+/* freqToCount function
+ * input  - frequency in Hertz
+ * sets global variable toneCount
+ */
+void playTone(unsigned int f){
+	toneCount = FREQ_SCALAR/f;
+}
 
 void main(void) {
 
@@ -76,31 +68,28 @@ void main(void) {
 
 	/* TIMER A0 SETUP */
 	TA0CTL	 = MC_1 | ID_2 | TASSEL_2;  // divide by 4
-	//TA0CCTL0 = CCIE;                  // Enable Timer A0 interrupts
+	TA0CCTL0 = CCIE;                    // Enable Timer A0 interrupts
 	TA0CCTL1 = OUTMOD_7;
-	TA0CCR0  = 0;
+	TA0CCR0  = 255;
 	TA0CCR1	 = 0;
 
 	/* Port 1 Setup */
-	P1DIR = BIT6;
+	P1DIR = BIT6 | LEDR;
 	P1OUT = 0;
 	P1SEL = BIT6;
 
+	__enable_interrupt();
+
 	while (1) {
+		playTone(220);
+		__delay_cycles(8000000);
 
-		/* Play A major! */
-		playTone(440, 16000); // A4  = 440Hz
-		playTone(494, 16000); // B4  = 494Hz
-		playTone(554, 16000); // C5# = 554Hz
-		playTone(587, 16000); // D5  = 587Hz
-		playTone(659, 16000); // E5  = 659Hz
-		playTone(740, 16000); // F5# = 740Hz
-		playTone(831, 16000); // G5# = 831Hz
+		playTone(440);
+		__delay_cycles(8000000);
 
-		stopTone();
-		__delay_cycles(4000000);
+		playTone(680);
+		__delay_cycles(8000000);
+
 	}
 }
-
-
 
